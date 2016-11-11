5f47952e6be0a864614c0612bc921fd6febf7662
KakanotCollaborator <seleniumcode.kaka@gmail.com>
2016-11-10 22:21:36 -0500

    Update WidgetTest.java

--- src/test/java/com/github/maven/example/tests/WidgetTest.java
+++ src/test/java/com/github/maven/example/tests/WidgetTest.java
@@ -32,4 +32,15 @@ public class WidgetTest {
 		widget.setEnabled(true);
 		assertTrue(widget.isEnabled());
 	}
+	@Test
+	public void updateEnableStateDup() {
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		Widget widget = new Widget();
+		widget.setEnabled(false);
+		assertFalse(widget.isEnabled());
+		widget.setEnabled(true);
+		assertTrue(widget.isEnabled());
+	}
 }
